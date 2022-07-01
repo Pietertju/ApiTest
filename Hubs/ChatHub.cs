21f93107d182052f78aacf1ed31baf3266a338ea
Pieter Ebbers <pieter.ebbers99@gmail.com>
2022-07-01 06:30:53 +0200

    Added websocket functionality

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ApiTest.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendConnectionId(string connectionId)
+        {
+            await Clients.All.SendAsync("setClientMessage", "A connection with ID '" + connectionId + "' has just connected");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,21 +1,21 @@
 using ApiTest;
 using ApiTest.Authentication;
 using ApiTest.Context;
+using ApiTest.Hubs;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using NSwag;
 using NSwag.Generation.Processors.Security;
 using System;
-using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 
@@ -52,6 +52,16 @@ namespace APItest
             })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            services.AddSignalR();
+
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder => {
+                builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000");
+            }));
 
             // Adding Authentication  
             services.AddAuthentication(options =>
@@ -119,7 +129,7 @@ namespace APItest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserContext userContext, TodoContext todoContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime hostApplicationLifetime, UserContext userContext, TodoContext todoContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -133,25 +143,32 @@ namespace APItest
             app.UseOpenApi();
             app.UseSwaggerUi3();
             app.UseHttpsRedirection();
-            
-            app.UseCors(x => x
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .SetIsOriginAllowed(origin => true) // allow any origin
-                            .AllowCredentials()
-                            .WithExposedHeaders("x-token-expired"));
 
+            app.UseCors("CorsPolicy");
+                            
             app.UseRouting();
 
+            hostApplicationLifetime.ApplicationStarted.Register(() =>
+            {
+
+                var serviceProvider = app.ApplicationServices;
+                var chatHub = (IHubContext<ChatHub>)serviceProvider.GetService(typeof(IHubContext<ChatHub>));
+
+                var timer = new System.Timers.Timer(1000);
+                timer.Enabled = true;
+                timer.Elapsed += delegate (object sender, System.Timers.ElapsedEventArgs e) {
+                    chatHub.Clients.All.SendAsync("setTime", DateTime.Now.ToString("dddd d MMMM yyyy HH:mm:ss"));
+                };
+                timer.Start();
+            });
 
             app.UseAuthentication();
-            app.UseAuthorization();
-
-            
+            app.UseAuthorization();  
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
